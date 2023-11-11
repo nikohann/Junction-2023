707a12b2d276e8f162bb56d803b1a040aae9e216
nikohann <niko@hannolainen.fi>
2023-11-11 10:49:20 +0200

    init

---
+++ components/navbar.tsx
@@ -0,0 +1,14 @@
+import { Navbar, NavbarBrand } from "@nextui-org/react";
+
+
+
+export function Chatbar() {
+
+    return (
+        <Navbar>
+            <NavbarBrand>
+                <h1>Chatbot</h1>
+            </NavbarBrand>
+        </Navbar>
+    )
+}
