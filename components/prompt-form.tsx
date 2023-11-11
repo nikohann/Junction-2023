f175adc9b867b23876aadf9316b09920fa9757e9
Daniel Tuukkanen <tuukkanen.daniel@gmail.com>
2023-11-11 18:55:17 +0200

    Rounds match with each other

--- components/prompt-form.tsx
+++ components/prompt-form.tsx
@@ -46,7 +46,7 @@ export function PromptForm({
         ref={formRef}
         className='w-full'
       >
-        <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background sm:rounded-md sm:border">
+        <div className="relative flex max-h-60 w-full grow flex-col overflow-hidden bg-background rounded-lg border">
 
           <Textarea
             ref={inputRef}
